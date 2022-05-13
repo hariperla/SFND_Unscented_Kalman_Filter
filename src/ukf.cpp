096280434c3166e75433da00507887a02a361f46
Hari Perla <haricharan.perla1988@gmail.com>
2022-05-13 09:55:36 -0400

    Prediction function complete
    1. Generate and augment sigma points
    2. Predict sigma points
    3. Predict mean state and covariance

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -170,6 +170,98 @@ void UKF::Prediction(double delta_t) {
    * and the state covariance matrix.
    */
 
+  /* Step 1 : Generate Sigma points for prediction */
+  // Create augmented state vector
+  VectorXd x_aug_ = VectorXd(n_aug_).setZero();
+  // Create augmented covariance matrix
+  MatrixXd P_aug_ = MatrixXd(n_aug_,n_aug_).setZero();
+  // Create augmented sigma point vector
+  MatrixXd x_sig_aug_ = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+
+  // Create augmented mean state
+  x_aug_.head(5) = x_;
+  x_aug_(5) = 0.0;
+  x_aug_(6) = 0.0;
+
+  // Create augmented covariance matrix
+  P_aug_.topLeftCorner(5,5) = P_;
+  P_aug_(5,5) = powSquare(std_a_);
+  P_aug_(6,6) = powSquare(std_yawdd_);
+
+  // Create the square root matrix
+  MatrixXd A = P_aug_.llt().matrixL();
+
+  // Generate augmented sigma points
+  x_sig_aug_.col(0) = x_;
+  for (int i = 0; i < n_aug_; ++i) 
+  {
+    x_sig_aug_.col(i+1) = x_ + sqrt(lambda_ + n_aug_) * A.col(i);
+    x_sig_aug_.col(i+1+n_aug_) = x_ - sqrt(lambda_ + n_aug_) * A.col(i);
+  }
+
+  /* Step 2 : Sigma point prediction */
+  for (int i = 0; i < 2 * n_aug_+ 1; ++i) 
+  {
+    // extract values for better readability
+    double px = x_sig_aug_(0,i);
+    double py = x_sig_aug_(1,i);
+    double v = x_sig_aug_(2,i);
+    double yaw = x_sig_aug_(3,i);
+    double yawd = x_sig_aug_(4,i);
+    double nu_a = x_sig_aug_(5,i);
+    double nu_yawdd = x_sig_aug_(6,i);
+
+    // predicted state values
+    double px_p, py_p;
+
+    // avoid division by zero
+    if (fabs(yawd) > 0.001) {
+        px_p = px + v / yawd * ( sin(yaw + yawd * delta_t) - sin(yaw) );
+        py_p = px + v / yawd * ( cos(yaw) - cos(yaw + yawd * delta_t) );
+    } else {
+        px_p = px + v * delta_t * cos(yaw);
+        py_p = py + v * delta_t * sin(yaw);
+    }
+
+    double v_p = v;
+    double yaw_p = yaw + yawd * delta_t;
+    double yawd_p = yawd;
+
+    // add noise
+    px_p = px_p + 0.5 * nu_a * powSquare(delta_t) * cos(yaw);
+    py_p = py_p + 0.5 * nu_a * powSquare(delta_t) * sin(yaw);
+    v_p = v_p + nu_a * delta_t;
+
+    yaw_p = yaw_p + 0.5 * nu_yawdd * powSquare(delta_t);
+    yawd_p = yawd_p + nu_yawdd * delta_t;
+
+    // write predicted sigma point into right column
+    Xsig_pred_(0,i) = px_p;
+    Xsig_pred_(1,i) = py_p;
+    Xsig_pred_(2,i) = v_p;
+    Xsig_pred_(3,i) = yaw_p;
+    Xsig_pred_(4,i) = yawd_p;
+  }
+
+  /* Step 3 : Predicting Mean and Covariance */
+  // Predict state mean
+  //x_.fill(0.0);
+  for (int i = 0; i < 2 * n_aug_ + 1; ++i) 
+  {  // iterate over sigma points
+    x_ = x_ + weights_(i) * Xsig_pred_.col(i);
+  }
+
+  // Predict covariance
+  for (int i = 0; i < 2 * n_aug_ + 1; ++i) 
+  { // iterate over sigma points
+    // state difference
+    VectorXd x_diff = Xsig_pred_.col(i) - x_;
+    // angle normalization
+    while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
+    while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+
+    P_ = P_ + weights_(i) * x_diff * x_diff.transpose() ;
+  }
 }
 
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
